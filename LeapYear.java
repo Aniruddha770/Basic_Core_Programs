233dcb087bf6face5af3af3c12a3126144723da7
Anirudha Patil <48207987+Aniruddha770@users.noreply.github.com>
2021-06-11 22:41:53 +0530

    Add files via upload

---
+++ LeapYear.java
@@ -0,0 +1,28 @@
+/**
+ * @author Aniruddha
+ * @since 11 Jun 2021
+ * Leap Year Program
+ */
+
+
+import java.util.Scanner;
+public class LeapYear {
+	public static void main(String[] args) {
+		Boolean flag = true;
+		System.out.println("Enter Year to check it leap or not");
+		Scanner sc= new Scanner(System.in);
+		int Year= sc.nextInt();
+		
+		if (Year%4 == 0 && Year%100 != 0 || Year%400 == 0) {
+			flag = true;
+			System.out.println(flag);
+		}
+		else {
+			flag = false;
+		System.out.println(flag);
+			
+		}
+		
+		}	
+		
+	}
